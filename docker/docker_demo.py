d97f5e177ae01eff47544278d251658b37f20ea3
JustusGammelgaard <justusgammelgaard@protonmail.com>
2020-06-08 14:18:30 +0200

    Changes not staged for commit: 	modified:   ../__pycache__/util.cpython-36.pyc 	modified:   Dockerfile 	deleted:    cfg/yolov3.cfg 	modified:   ../download_models.sh 	modified:   ../image_demo.py

--- docker/docker_demo.py
+++ docker/docker_demo.py
@@ -4,7 +4,7 @@ import os
 import time
 
 if __name__ == "__main__":
-    net = Detector(bytes("cfg/yolov3.cfg", encoding="utf-8"), bytes("weights/yolov3.weights", encoding="utf-8"), 0, bytes("cfg/coco.data",encoding="utf-8"))
+    net = Detector(bytes("cfg/yolov123.cfg", encoding="utf-8"), bytes("weights/yolov123.weights", encoding="utf-8"), 0, bytes("cfg/coco.data",encoding="utf-8"))
 
     input_files = os.listdir("input")
     for file_name in input_files:
