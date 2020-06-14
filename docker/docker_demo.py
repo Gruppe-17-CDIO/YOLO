c5cab8c115e335ee78aa0aaae8700b638a20aa12
JustusGammelgaard <justusgammelgaard@protonmail.com>
2020-06-14 22:16:37 +0200

    fixed png

--- docker/docker_demo.py
+++ docker/docker_demo.py
@@ -5,7 +5,7 @@ import time
 import json
 
 if __name__ == "__main__":
-    net = Detector(bytes("cfg/yolov123.cfg", encoding="utf-8"), bytes("weights/yolov300.weights", encoding="utf-8"), 0, bytes("cfg/coco.data",encoding="utf-8"))
+    net = Detector(bytes("cfg/yolov123.cfg", encoding="utf-8"), bytes("weights/yolov123.weights", encoding="utf-8"), 0, bytes("cfg/coco.data",encoding="utf-8"))
 
     input_files = os.listdir("docker/input")
     for file_name in input_files:
