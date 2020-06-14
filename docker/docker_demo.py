c6fc6ade4018dc172dbeb82f8fd21feba0bd9433
JustusGammelgaard <justusgammelgaard@protonmail.com>
2020-06-14 22:53:54 +0200

    fix

--- docker/docker_demo.py
+++ docker/docker_demo.py
@@ -30,6 +30,6 @@ if __name__ == "__main__":
             #cv2.putText(img,str(cat.decode("utf-8")),(int(x),int(y)),cv2.FONT_HERSHEY_DUPLEX,4,(0,0,255), thickness=2)
         cv2.imwrite(os.path.join("output",file_name), img)
         with open("json_out.txt", "w") as file:
-	    for item in res:
-		file.write("%s\n", item)
+            for item in res:
+                file.write("%s\n", item)
         print()
